12c36a3e8007afa5e8a4d9ca8251321e9c8295df
Michael McDonagh <mcdonagh@msi.localdomain>
2019-02-17 03:43:39 +0000

    Created art piece structure and basic functions for adding and inserting into the linked list.

--- main.c
+++ main.c
@@ -1,5 +1,48 @@
 #include <stdio.h>
 
+typedef struct ArtPiece {
+	ArtPiece *prev;
+	ArtPiece *next;
+
+	int id;
+	char* art_type;
+	char* art_name;
+	char* artist_name;
+	int price;	
+} *head, *tail;
+
+ArtPiece *createArtPiece(int id, char* art_type, char* art_name, char* artist_name, int price){
+	ArtPiece output;
+	output.id = id;
+	output.art_type = art_type;
+	output.art_name = art_name;
+	output.artist_name = artist_name;
+	output.price = price;
+	return *output;
+}
+
+bool empty = true;
+
+void insertNewArtPiece(ArtPiece *app){
+	if (empty){
+		head = app;
+		tail = app;
+		(&app).prev = head;
+		(&app).next = tail;
+		empty = false;
+	}
+	else{
+		ArtPiece *cursor = head;
+		while(((&app).id > (&(&cursor).next).id) && (cursor != tail)){
+			cursor = (&cursor).next;
+		}
+		(&app).prev = cursor;
+		(&app).next = (&cursor).next;
+		(&cursor).next = app;
+		(&(&app).next).prev = app;
+	}
+}
+
 int main(int argc, char** argv) {
       /*
        *   * Dummy values
@@ -11,10 +54,11 @@ int main(int argc, char** argv) {
 	int price = 100;
 
 	/*
-    * This formatting for the string
-    * that you are expected to follow
-    */
-    printf("%d %s %s %s %d\n", id, art_type, art_name, artist_name, price);
+    	* This formatting for the string
+    	* that you are expected to follow
+    	*/
+	
+	printf("%d %s %s %s %d\n", id, art_type, art_name, artist_name, price);
 
 	return 0;
 }
